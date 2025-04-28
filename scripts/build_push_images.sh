79d5965d0f46ef513d92a301b59916646ba92998
Deven varu <103780082+Devenvaruv@users.noreply.github.com>
2025-04-27 17:20:29 -0700

    Update build_push_images.sh

--- scripts/build_push_images.sh
+++ scripts/build_push_images.sh
@@ -9,8 +9,8 @@ DATE_TAG=$(date +"%Y%m%d")
 for SERVICE in "${SERVICES[@]}"
 do
   echo "🚀 Building $SERVICE ..."
-  docker build -t "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-$DATE_TAG" ./devenops-source/$SERVICE
+  docker build -t "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-20250427" ./devenops-source/$SERVICE
   echo "📤 Pushing $SERVICE to ECR ..."
-  docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-$DATE_TAG"
+  docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-20250427"
   echo "✅ Finished $SERVICE"
 done
