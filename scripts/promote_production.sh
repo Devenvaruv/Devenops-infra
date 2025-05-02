c0f054d3b6e06ca5b4c82da9a1581f08407064b5
Devenvaruv <devenvaruv@gmail.com>
2025-05-02 16:56:05 -0700

    added promotion logic

--- scripts/promote_production.sh
+++ scripts/promote_production.sh
@@ -4,7 +4,7 @@ set -e
 DATE_TAG=$(date +"%Y%m%d")
 ACCOUNT_ID=$1
 OLD_TAG=$2        
-SOURCE_TAG="$NEW_TAG-$DATE_TAG"      
+SOURCE_TAG="$OLD_TAG-$DATE_TAG"      
 REGION="us-east-1"
 REPOSITORIES=("frontend" "auth-service")
 
