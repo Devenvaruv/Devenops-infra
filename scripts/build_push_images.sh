ef813afdb6be587bc0a2cac294be4f1faa994b55
Devenvaruv <devenvaruv@gmail.com>
2025-05-03 12:39:25 -0700

    updated script to build only changed services

--- scripts/build_push_images.sh
+++ scripts/build_push_images.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # scripts/build_push_images.sh
-
 set -e
 
-SERVICES=("frontend" "auth-service" )
+SERVICES=("frontend" "auth-service")
 DATE_TAG=$(date +"%Y%m%d")
 
-for SERVICE in "${SERVICES[@]}"
-do
-  echo "🚀 Building $SERVICE ..."
-  docker build -t "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-$DATE_TAG" ./devenops-source/$SERVICE
-  echo "📤 Pushing $SERVICE to ECR ..."
-  docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-$DATE_TAG"
-  echo "✅ Finished $SERVICE"
+# Gets list of changed directories in devenops-source/
+CHANGED_SERVICES=$(git -C devenops-source diff --name-only HEAD~1 HEAD | awk -F/ '{print $1}' | sort -u)
+
+for SERVICE in "${SERVICES[@]}"; do
+  if echo "$CHANGED_SERVICES" | grep -q "^$SERVICE$"; then
+    echo "🚀 Changes detected in $SERVICE, building image..."
+    docker build -t "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-$DATE_TAG" ./devenops-source/$SERVICE
+    docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$SERVICE:nightly-$DATE_TAG"
+    echo "✅ $SERVICE pushed"
+  else
+    echo "⏭️ No changes in $SERVICE, skipping build"
+  fi
 done
